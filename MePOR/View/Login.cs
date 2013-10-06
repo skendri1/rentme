75ea59e79c47791b0f00e2abee7288d3c3b5d461
drew <devnull@localhost>
2013-10-06 16:45:13 -0400

    Created invalid login diag. Still cannot login.

--- MePOR/View/Login.cs
+++ MePOR/View/Login.cs
@@ -1,25 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MePOR.View;
 
 namespace MePOR
 {
     public partial class Login : Form
     {
+        private string _username;
+        private string _password;
+
+        private const string EmployeeUsername = "employee";
+        private const string AdminUsername = "admin";
+
+        private Dictionary<string, string> userPasswords;
+
         public Login()
         {
             InitializeComponent();
+            LoadUsers();
         }
 
         private void LoginButtonClick(object sender, EventArgs e)
         {
+            this._username = this.usernameTextbox.Text;
+            this._password = this.passwordTextbox.Text;
 
+            if(this.userPasswords.ContainsKey(this._username))
+            {
+                string usernamesCorrectPassword;
+                this.userPasswords.TryGetValue(this._username, out usernamesCorrectPassword);
+                
+                CheckPassword(usernamesCorrectPassword);
+            }
+            else
+            {
+                ShowInvalidDialog();
+            }
+
+        }
+
+        private void CheckPassword(string usernamesCorrectPassword)
+        {
+            if (usernamesCorrectPassword != null && usernamesCorrectPassword.Equals(this._password))
+            {
+                MePOR meporApplication = new MePOR();
+                meporApplication.ShowDialog(this);
+                meporApplication.Dispose();
+            }
+            else
+            {
+                ShowInvalidDialog();
+            }
+        }
+
+        private void ShowInvalidDialog()
+        {
+            Invalid invalidDialog = new Invalid();
+            invalidDialog.ShowDialog(this);
+            invalidDialog.Dispose();
+        }
+
+        private void LoadUsers()
+        {
+            this.userPasswords = new Dictionary<string, string> { { EmployeeUsername, _password }, { AdminUsername, _password } };
         }
     }
 }
