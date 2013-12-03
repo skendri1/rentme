7fe50c2f8e44b65dc588c1656603bba92e73942a
drew <devnull@localhost>
2013-12-03 13:34:48 -0500

    Added a few more header comments.

--- MePOR/Controller/DBAccessController.cs
+++ MePOR/Controller/DBAccessController.cs
@@ -13,10 +13,19 @@ namespace MePOR.Controller
     {
         Database db;
 
+        /// <summary>
+        /// Creates a new DBAccessControll that provides the GUI and interface to the database.
+        /// </summary>
         public DBAccessController(){
             db = new Database();
         }
         
+        /// <summary>
+        /// Gets the ID of the employee with specified username and password.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>employee ID number</returns>
         public int GetCurrentEmployeeID(string username, string password)
         {
             DataTable dt = this.db.QueryEmployeeLoginID(username, password);
@@ -25,16 +34,31 @@ namespace MePOR.Controller
             return id;
         }
 
+        /// <summary>
+        /// Returns an item with specified criteria and search keyword.
+        /// </summary>
+        /// <param name="searchCriteria"></param>
+        /// <param name="search"></param>
+        /// <returns>returns all items with specified criteria and keyword</returns>
         public DataTable SearchItem(string searchCriteria, string search)
         {
             return this.db.SearchItem(searchCriteria, search);
         }
 
+        /// <summary>
+        /// Provides an interface for the administrator to perform an advanced query on the database.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>DataTable with the results of the advanced query.</returns>
         public DataTable PerformAdvancedQuery(string query)
         {
             return this.db.PerformAdvancedQuery(query);
         }
 
+        /// <summary>
+        /// Test to see if there is a valid connection to the database.
+        /// </summary>
+        /// <returns>Boolean indicating a connection with the database</returns>
         public bool CanConnectToDB()
         {
             return this.db.CanConnectToServer();
@@ -42,11 +66,22 @@ namespace MePOR.Controller
 
         #region SEARCH CUSTOMER
 
+        /// <summary>
+        /// Searches the database for members with specified name.
+        /// </summary>
+        /// <param name="fname">first name of member</param>
+        /// <param name="lname">last name of member</param>
+        /// <returns>DataTable of all members with specified name</returns>
         public DataTable SearchCustomerByName(string fname, string lname)
         {
             return this.db.SearchCustomerByName(fname, lname);
         }
 
+        /// <summary>
+        /// Searches the database for all members with specified phone number.
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns>DataTable of all members with specified phone number.</returns>
         public DataTable SearchCustomerByPhone(string phoneNumber)
         {
             return this.db.SearchCustomerByPhone(phoneNumber);
